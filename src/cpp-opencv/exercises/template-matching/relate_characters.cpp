07dc44f0287aa48d9670e6a2ff04cae110ef8fa2
AndreMorenoGoveia <andremorgov@gmail.com>
2026-03-15 22:21:52 -0300

    feat: add relate characters exercise

--- src/cpp-opencv/exercises/template-matching/relate_characters.cpp
+++ src/cpp-opencv/exercises/template-matching/relate_characters.cpp
@@ -1,11 +1,64 @@
 #include "procimagem.h"
 
-Mat_<float> rotacao(Mat_<float> ent, double graus, Point2f centro, Size tamanho)
+const float DONTCARE = 128.0f / 255.0f;
+
+Size tamanhoRotacaoCompleta(Size original)
+{
+    int lado = cvRound(ceil(sqrt(original.width * original.width +
+                                  original.height * original.height)));
+    int largura = lado;
+    int altura = lado;
+
+    if ((largura - original.width) % 2 != 0)
+        largura++;
+    if ((altura - original.height) % 2 != 0)
+        altura++;
+
+    return Size(largura, altura);
+}
+
+Mat_<float> centralizaEmCanvas(Mat_<float> ent, Size tamanho, float fundo)
+{
+    Mat_<float> sai(tamanho, fundo);
+    int x = (tamanho.width - ent.cols) / 2;
+    int y = (tamanho.height - ent.rows) / 2;
+    Mat_<float>(sai, Rect(x, y, ent.cols, ent.rows)) = ent;
+    return sai;
+}
+
+Mat_<float> centralizaMascara(Size original, Size tamanho)
+{
+    Mat_<float> sai(tamanho, 0.0f);
+    int x = (tamanho.width - original.width) / 2;
+    int y = (tamanho.height - original.height) / 2;
+    Mat_<float>(sai, Rect(x, y, original.width, original.height)) = 1.0f;
+    return sai;
+}
+
+Mat_<float> rotacao(Mat_<float> ent, double graus, Point2f centro, Size tamanho, float fundo)
 {
     Mat_<double> m = getRotationMatrix2D(centro, graus, 1.0);
     Mat_<float> sai;
-    warpAffine(ent, sai, m, tamanho, INTER_LINEAR, BORDER_CONSTANT, Scalar(1.0));
+    warpAffine(ent, sai, m, tamanho, INTER_LINEAR, BORDER_CONSTANT, Scalar(fundo));
     return sai;
+}
+
+Mat_<float> rotacaoMascara(Mat_<float> ent, double graus, Point2f centro, Size tamanho)
+{
+    Mat_<double> m = getRotationMatrix2D(centro, graus, 1.0);
+    Mat_<float> sai;
+    warpAffine(ent, sai, m, tamanho, INTER_NEAREST, BORDER_CONSTANT, Scalar(0.0f));
+    return sai;
+}
+
+Rect boundingRectMascara(const Mat_<float> &mascara)
+{
+    Mat_<uchar> valida = (mascara > 0.5f);
+    vector<Point> pontos;
+    findNonZero(valida, pontos);
+    if (pontos.empty())
+        return Rect(0, 0, mascara.cols, mascara.rows);
+    return boundingRect(pontos);
 }
 
 void marca(Mat_<Vec3b> &destino, Point2f centro, Size patternSize, double angulo, int indice)
@@ -25,16 +78,15 @@ void marca(Mat_<Vec3b> &destino, Point2f centro, Size patternSize, double angulo
             FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0, 255, 0), 2, 8);
 }
 
-void match_images(Mat_<uchar> ent, int index)
+void match_images(Mat_<float> ent, int index)
 {
+    Mat_<uchar> ent8u;
+    ent.convertTo(ent8u, CV_8U, 255.0);
+
     Mat_<Vec3b> resultado;
-    cvtColor(ent, resultado, COLOR_GRAY2BGR);
+    cvtColor(ent8u, resultado, COLOR_GRAY2BGR);
 
-    ent = ent / 255.0;
-
-    return;
-
-
+    imwrite("results/resultado.jpg", resultado);
 
     for (int i = 1; i <= 12; i++)
     {
@@ -49,14 +101,25 @@ void match_images(Mat_<uchar> ent, int index)
         Mat_<float> pattern = imread(path, 0);
         pattern = pattern / 255.0;
 
-        Point2f centro(pattern.cols / 2.0f, pattern.rows / 2.0f);
+        Size tamanhoRot = tamanhoRotacaoCompleta(pattern.size());
+        Mat_<float> patternExpandido = centralizaEmCanvas(pattern, tamanhoRot, DONTCARE);
+        Mat_<float> mascaraExpandida = centralizaMascara(pattern.size(), tamanhoRot);
+        Point2f centro(tamanhoRot.width / 2.0f, tamanhoRot.height / 2.0f);
         double melhorValor = -DBL_MAX;
         Point melhorPosicao;
         int melhorAngulo = 0;
 
-        for (int angulo = 0; angulo < 360; angulo += 30) {
-            Mat_<float> rotacionado = rotacao(pattern, angulo, centro, pattern.size());
-            Mat_<float> p = matchTemplateSame(ent, rotacionado, TM_CCOEFF_NORMED);
+        for (int angulo = 0; angulo < 360; angulo-=-5) {
+            Mat_<float> rotacionado = rotacao(patternExpandido, angulo, centro, tamanhoRot, DONTCARE);
+            Mat_<float> mascaraRotacionada = rotacaoMascara(mascaraExpandida, angulo, centro, tamanhoRot);
+            Rect roiValida = boundingRectMascara(mascaraRotacionada);
+            Mat_<float> rotacionadoRecortado = rotacionado(roiValida).clone();
+            Mat_<float> mascaraRecortada = mascaraRotacionada(roiValida).clone();
+            Mat_<uchar> pixelsInvalidos = (mascaraRecortada <= 0.5f);
+            rotacionadoRecortado.setTo(DONTCARE, pixelsInvalidos);
+
+            Mat_<float> rotacionadoPrep = somaAbsDois(dcReject(rotacionadoRecortado, DONTCARE));
+            Mat_<float> p = filtro2d(ent, rotacionadoPrep);
 
             double maxVal;
             Point maxLoc;
@@ -68,8 +131,7 @@ void match_images(Mat_<uchar> ent, int index)
             }
         }
 
-        if (melhorValor >= 0.9)
-            marca(resultado, Point2f(melhorPosicao.x, melhorPosicao.y), pattern.size(), melhorAngulo, i);
+        marca(resultado, Point2f(melhorPosicao.x, melhorPosicao.y), pattern.size(), melhorAngulo, i);
     }
     imwrite("results/resultado_" + to_string(index) + ".jpg", resultado);
 }
@@ -79,7 +141,8 @@ int main()
 
     for (int i = 1; i <= 8; i++)
     {
-        Mat_<uchar> ent = imread("assets/extra_a" + to_string(i) + ".jpg", 0);
+        Mat_<float> ent = imread("assets/extra_a" + to_string(i) + ".jpg", 0);
+        ent = ent / 255.0;
         match_images(ent, i);
     }
 }
