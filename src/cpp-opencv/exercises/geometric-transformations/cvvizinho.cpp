a1abb09177d25492f4ad73b9c494d6bb9e1423a2
AndreMorenoGoveia <andremorgov@gmail.com>
2026-03-17 14:33:18 -0300

    feat: alterations into cvvizinho

--- src/cpp-opencv/exercises/geometric-transformations/cvvizinho.cpp
+++ src/cpp-opencv/exercises/geometric-transformations/cvvizinho.cpp
@@ -2,8 +2,9 @@
 #include "procimagem.h"
 int main() {
   Mat_<uchar> a=imread("assets/lennag.jpg",0); if (a.total()==0) erro("Erro leitura");
-  double fator=1.5;
+  double fatorLinha=1.5;
+  double fatorColuna=2;
   Mat_<uchar> b;
-  resize(a, b, Size(0,0), fator, fator, INTER_NEAREST);
+  resize(a, b, Size(0,0), fatorLinha, fatorColuna, INTER_NEAREST);
   imwrite("results/cvvizinho.jpg",b);
 } 
